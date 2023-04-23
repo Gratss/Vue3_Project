242ebb7b8288bee69c0814cbb180227ff0e7285e
Gratss <snaelzzz@gmail.com>
2023-04-23 13:32:27 +0300

    2.3

---
+++ frontend/src/App.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+    };
+  },
+};
+</script>
+
+<template>
+  <v-app>
+    <v-navigation-drawer app v-model="drawer"></v-navigation-drawer>
+    <v-app-bar app dark color="primary">
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn>
+          <v-icon start icon="mdi-train-variant"></v-icon>
+          Link One
+        </v-btn>
+        <v-btn>Link Two</v-btn>
+        <v-btn>Link Three</v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+    <v-main></v-main>
+  </v-app>
+</template>
