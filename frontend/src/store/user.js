bdeb6be9a617f12f6babb5057c835bbd5a057e85
Gratss <snaelzzz@gmail.com>
2023-04-23 17:51:40 +0300

    12.2+12.3

--- frontend/src/store/user.js
+++ frontend/src/store/user.js
@@ -10,3 +10,25 @@ export default {
 	}
 }
 }
+class User {
+    constructor(id, email, password) {
+    this.id = id
+    this.email = email
+    this.password = password
+    }
+    }
+    mutations: {
+        setUser(state,payload) 
+        {
+        console.log(payload)
+        state.user = payload
+        }
+        }
+        actions: {
+        registerUser({commit},{email, password})
+        {
+        //Здесь запрос на сервер для регистрации
+        commit('setUser', new User(1, email, 	password))
+        }
+        }
+            
