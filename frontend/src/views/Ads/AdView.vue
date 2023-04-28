7ec7dcad922e74bfb23f4aa03b241ffd728a1b88
Gratss <snaelzzz@gmail.com>
2023-04-28 20:52:33 +0300

    17.4-17.6

--- frontend/src/views/Ads/AdView.vue
+++ frontend/src/views/Ads/AdView.vue
@@ -1,35 +1,42 @@
 <template>
-  <v-container>
-      <v-row>
-          <v-col cols="12">
-              <v-card class="mt-5">
-                  <v-img
-                  height="300px"
-                  :src="ad.src"
-                  ></v-img>
-                  <v-card-text>
-                  <h1 class="text--primary mb-3">{{ ad.title }}</h1>
-                  <p>{{ ad.desc }}</p>
-                  </v-card-text>
-                  <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn text class="warning">Изменить</v-btn>
-                  <v-btn class="success">Купить</v-btn>
-                  </v-card-actions>	
-              </v-card>
-          </v-col>
-      </v-row>
-  </v-container>
+    <v-container>
+        <v-row>
+            <v-col cols="12">
+                <v-card class="mt-5">
+                    <v-img
+                    height="300px"
+                    :src="ad.src"
+                    ></v-img>
+                    <v-card-text>
+                    <h1 class="text--primary mb-3">{{ ad.title }}</h1>
+                    <p>{{ ad.desc }}</p>
+                    </v-card-text>
+                    <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <modal-dialog :ad="ad" v-if="isOwner"></modal-dialog>
+                    <v-btn class="success">Купить</v-btn>
+                    </v-card-actions>	
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
+import EditAdModal from './EditAdModal'
 export default {
-  props: ['id'],
-  computed: {
-      ad() {
-          const id = this.id
-          return this.$store.getters.adById(id)
-      }
-  }
+    props: ['id'],
+    computed: {
+        ad() {
+            const id = this.id
+            return this.$store.getters.adById(id)
+        }
+    },
+    components: {
+		'modal-dialog': EditAdModal
+    },
+    isOwner () {
+	return this.ad.ownerId === 	this.$store.getters.user.id
+    }
 }
 </script>
