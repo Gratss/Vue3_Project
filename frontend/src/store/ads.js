0b35211b5f77b4b48e1a6d3a4602708c40f6341d
Gratss <snaelzzz@gmail.com>
2023-04-28 20:30:51 +0300

    16.4

--- frontend/src/store/ads.js
+++ frontend/src/store/ads.js
@@ -82,9 +82,11 @@ export default {
           return ad.promo
         })
       },
-      myAds(state) {
-        return state.ads
-    },
+      myAds(state, getters) {
+  return state.ads.filter(ad => {
+  return ad.userId == getters.user.id
+  })
+  },
       adById(state) {
     return id => {
     return state.ads.find(ad => ad.id == id)
